7d1fdee12bdf8aff32d7ba9b9b992a89ee2fb459
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-14 19:04:54 +0300

    Create Observation.cs; represents bird observation object
    These objects are to be displayed in  the MainPage view once created. They are created in the NewObservationPage view and sent to be added to the MainPage view Observations list on clicking save button in NewObservationPage.

---
+++ BirdWatcher/BirdWatcher/Observation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirdWatcher
+{
+    public class Observation
+    {
+        public readonly string Species = "";
+        public readonly string Notes = "";
+        public readonly string Rarity = "";
+
+        public Observation(string species, string notes, string rarity)
+        {
+            Species = species;
+            Notes = notes;
+            Rarity= rarity;
+        }
+    }
+}

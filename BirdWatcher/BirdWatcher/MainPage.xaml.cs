060af56ed8b051baa2b19b0136c1b247ef8fd3ef
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-14 19:07:20 +0300

    Create Observation object list, function to add objects to list called from NewObservationPage save

--- BirdWatcher/BirdWatcher/MainPage.xaml.cs
+++ BirdWatcher/BirdWatcher/MainPage.xaml.cs
@@ -9,12 +9,18 @@ namespace BirdWatcher
 {
     public partial class MainPage : ContentPage
     {
+        public static List<Observation> Observations = new List<Observation>();
+
         public MainPage()
         {
             InitializeComponent();
+        // Called from NewObservationPage on saving new Observation object
+        public static void AddObservationToList(Observation newObservation)
+        {
+            Observations.Add(newObservation);
+            Console.WriteLine($"Added Observation object {newObservation.Species} to list (FIRST TEST: {Observations[0].Species})");
         }
-        
-        // Methods called by button clicks from MainPage view
+        // Method called by button clicks from MainPage view
         async void OnButtonNewObservationClicked(object sender, EventArgs args)
         {
             await Navigation.PushAsync(new NewObservationPage());

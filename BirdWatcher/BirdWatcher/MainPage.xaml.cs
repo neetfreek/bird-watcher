c87472ea7845d2e0ed6570b5f38780ff07f13adc
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-15 12:23:19 +0300

    Programatically create label children for ObservationStackLayout StackLayout
    This generates a list of vertically-aligned labels with text currently set to species name of Observations (if any).
    Currently does not remove the old list; i.e. adding all again each time a new observation is added.
    TODO: Clear pre-exisiting labels before adding or skip already added labels.

--- BirdWatcher/BirdWatcher/MainPage.xaml.cs
+++ BirdWatcher/BirdWatcher/MainPage.xaml.cs
@@ -16,20 +16,36 @@ namespace BirdWatcher
         {
             InitializeComponent();
             observations = ObservationSerialization.LoadObservationListFromPreferences();
+            UpdateMainPageStackLayout();        
 
             // Setup message subscriber for notification all Observation object data sent
             MessagingCenter.Subscribe<NewObservationPage>(this, "DataSaved", (sender) =>
             {
                 observations = ObservationSerialization.LoadObservationListFromPreferences();
+                UpdateMainPageStackLayout();
             });
         }
 
+        private void UpdateMainPageStackLayout()
         {
-            Observation newObservation = new Observation(species, notes, rarity);
-            Console.WriteLine($"CREATED new Observation object {newObservation.Species}");
-
-            Observations.Add(newObservation);
+            // REMOVE OR SKIP PRE-EXISTING LABELS IN MAINPAGESTACKLAYOUT
+            CreateObservationLabels();
         }
+
+        private void CreateObservationLabels()
+        {
+            for (int counter = 0; counter < observations.Count; counter++)
+            {
+                Label label = new Label() {
+                    Text = observations[counter].Species,
+                    FontSize = 20,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                    VerticalOptions = LayoutOptions.Start
+                };
+                ObservationStackLayout.Children.Add(label);
+            }
+        }
+
         // Method called by button clicks from MainPage view
         async void OnButtonNewObservationClicked(object sender, EventArgs args)
         {

4e4b24d1b0d08218a7df1c52258e5af3272bdbb3
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-15 11:19:39 +0300

    Define data contract for Observation objects
    By assigning DataContract and DataMember attributes to Object, fields respectively, we can serialize Observation objects into JSON with a DataContractSerializer. This allows for saving of Observation objects as strings in Preferences (nice as supported across platforms), and loading Observation data from Preferences and re-converting into objects.
    
    These objects are used to populate values of UI elements which appear to user in MainPage  view.

--- BirdWatcher/BirdWatcher/Observation.cs
+++ BirdWatcher/BirdWatcher/Observation.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace BirdWatcher
 {
+    [DataContract]
     public class Observation
     {
+        [DataMember]
         public readonly string Species = "";
+        [DataMember]
         public readonly string Notes = "";
+        [DataMember]
         public readonly string Rarity = "";
 
         public Observation(string species, string notes, string rarity)

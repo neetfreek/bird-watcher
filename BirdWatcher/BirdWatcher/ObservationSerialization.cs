b3a4043b9a4fc9cbe4cb0d45f16aa73ec8760485
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-08-15 11:22:20 +0300

    Create ObservationSerialization.cs
    Responsible for: 1) SAVING Observation object data to Preferences via converting to JSON and 2) LOADING by re-creating Observation objects from Preferences data and returning a list Observation objects.

---
+++ BirdWatcher/BirdWatcher/ObservationSerialization.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// For object serialization and saving data
+using System.IO;
+using System.Runtime.Serialization.Json;
+using Xamarin.Essentials;
+
+namespace BirdWatcher
+{
+    public static class ObservationSerialization
+    {
+        // Handle convert Observation object to JSON data, save to Preferences
+        public static void SaveObservationToPreferences(Observation observation)
+        {
+            string newObservationString = ObservationToString(observation);
+            AddObservationStringToPreferences(newObservationString);
+        }
+
+        // Handle create Observation objects from Preferences JSON data, add to list, return
+        public static List<Observation> LoadObservationListFromPreferences()
+        {
+            List<Observation> observations = new List<Observation>();
+
+            int counter = 0;
+            while (Preferences.ContainsKey(counter.ToString()))
+            {
+                Observation observationToAdd = PreferencesDataToObject(Preferences.Get(counter.ToString(), "NOT FOUND"));
+                observations.Add(observationToAdd);
+                counter++;
+            }
+
+            return observations;
+        }
+
+
+        // Create JSON object from Observation data
+        private static string ObservationToString(Observation observation)
+        {
+            MemoryStream stream = new MemoryStream();
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Observation));
+            serializer.WriteObject(stream, observation);
+
+            byte[] observationJson = stream.ToArray();
+            stream.Close();
+            string observationString = Encoding.UTF8.GetString(observationJson, 0, observationJson.Length);
+
+            return observationString;
+        }
+
+        // Add string-format observation data (value) as string to preferences with object name as key
+        private static void AddObservationStringToPreferences(string newObservationString)
+        {
+            int counter = 0;
+            // Create unique key for observation
+            while (Preferences.ContainsKey($"{counter}"))
+            {
+                counter++;
+            }
+
+            Preferences.Set($"{counter}", newObservationString);
+        }
+
+        // Return Observation object with fields set from Preferences data
+        private static Observation PreferencesDataToObject(string observationString)
+        {
+            Observation observation = new Observation("template", "template", "template");
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(observationString));
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(observation.GetType());
+
+            return serializer.ReadObject(stream) as Observation;
+        }
+    }
+}
